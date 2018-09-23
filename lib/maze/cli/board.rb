72c6415fae02078d4a371e5c472d4177cb301307
Metin Yorulmaz <metin@me.com>
2018-09-23 14:48:14 +0300

    Refactor display method

--- lib/maze/cli/board.rb
+++ lib/maze/cli/board.rb
@@ -1,68 +1,71 @@
 module Maze
   class Board
-    attr_accessor :start, :finish
-    attr_accessor :width, :height, :board
+    attr_accessor :start, :finish, :current
+    attr_accessor :width, :height, :board, :stack
 
     def initialize(w, h)
       @width = w
       @height = h
-      @board = [] #Array.new(w) { Array.new(h) }
-      @start = Cell.new(0, 0)
-      @finish = Cell.new(w, h)
+      @board = []
       generate
+      @stack = []
     end
 
     def generate
-      0.upto(width) do |x|
+      0.upto(width - 1) do |x|
           board << []
-          0.upto(height) do |y|
-            board[x][y] = Cell.new(x, y)
+          0.upto(height - 1) do |y|
+            board[x][y] = Cell.new(x, y, self)
           end
       end
+      @start = cell(0, 0)
+      @finish = cell(width, height)
+
     end
+    private :generate
+
 
     def display
       # Each cell is printed as 9 places on console
-      0.upto(height) do |y|
+      0.upto(height - 1) do |y|
 
         # Print top line and
         if y == 0
-          0.upto(width) do |x|
-            print " " if x == 0
-            print board[x][y].top == 1 ? "─" : " "
-            print " "
+          0.upto(width - 1) do |x|
+            print " " if x == 0 
+            print "─ "
           end
           puts
         end
     
-        0.upto(width) do |x|
-          if x == 0
-            print board[x][y].left == 1 ? "|" : " "
-          end
+        0.upto(width - 1) do |x|
+          print "|" if x == 0 #left wall for the maze
           print print_cell(board[x][y])
           print board[x][y].right == 1 ? "|" : " "
         end
         puts
     
-        0.upto(width) do |x|
-          print " " if x == 0
+        0.upto(width - 1) do |x|
+          print " " if x == 0 # bottom left corner of each cell in first col
           print board[x][y].bottom == 1 ? "─" : " "
-          print " "
+          print " " # bottom right corner of each cell
         end
         puts
       end
     end
 
-    def print_cell(current_cell)
-      if current_cell == start
-        "s"
-      elsif current_cell == finish  
-        "f"
+    def print_cell(cell)
+      if current && cell == current  
+        "■"
       else
-        current_cell.val
+        cell.display 
       end
     end
 
+    def cell(x, y)
+      board[x] && board[x][y]
+    end
+    
   end
 end
   
--- spec/maze/board_spec.rb
+++ spec/maze/board_spec.rb
@@ -1,26 +1,42 @@
 RSpec.describe Maze::Board do
   let(:with) { 10 }
   let(:height) { 6 }
-  let(:board) { Maze::Board.new(with, height) }
+  let(:maze) { Maze::Board.new(with, height) }
   it "exists " do
-    expect(board).not_to be nil
+    expect(maze).not_to be nil
   end
 
-  it "display the board" do
-    board.generate
-    board.display
+  it "displays the maze" do
+    maze.display
+  end
+
+  describe :cell do
+    it "returns cell if in the bounds" do
+      expect(maze.cell(2, 2)).not_to eq(nil)
+    end
+
+    it "returns nil if not in the bounds" do
+      expect(maze.cell(with, height)).to eq(nil)
+    end
+    
+    it "returns nil if not in the bounds" do
+      expect(maze.cell(2, 200)).to eq(nil)
+    end
   end
 
   describe :print_cell do
-    it "marks start cell with s" do
-      start_cell =  Maze::Cell.new(0, 0)
-      expect(board.print_cell(start_cell)).to eq("s")
+    it "marks current cell with ■" do
+      cell = maze.cell(5, 5)
+      maze.current = cell
+      expect(maze.print_cell(cell)).to eq("■")
     end
 
-    it "marks finish cell with f" do
-      finish_cell =  Maze::Cell.new(with, height)
-      expect(board.print_cell(finish_cell)).to eq("f")
+    it "marks any other cell with cells display method" do
+      cell = maze.cell(1, 2)
+      expect(maze.print_cell(cell)).to eq(" ")
     end
   end
+
+
 
 end
